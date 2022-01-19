c741ade4e092223a95e01c82b3c87086ce64ec6f
ctninman <89664157+ctninman@users.noreply.github.com>
2022-01-19 15:19:07 -0500

    commit tree 1376f123179bfc70e9072e2b70ab7120da6dfd16

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,20 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :id, :name, :grade
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    student = self.new(id, name, grade)
+  end
+
+  def initialize(id: nil, name:, grade:)
+    @id = id 
+    @name = name
+    @grade = grade
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
